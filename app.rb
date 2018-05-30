1e5631b3a8086806b2ca6433950553bc050dc5af
Simone <simonecaplin@gmail.com>
2018-05-30 20:50:17 +0100

    add play.erb so users can enter their names

--- app.rb
+++ app.rb
@@ -2,7 +2,14 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
   get '/' do
-    'Testing infrastructure working!'
+    erb :index
+    # 'Testing infrastructure working!'
+  end
+
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
   end
 
   # start the server if ruby file executed directly
