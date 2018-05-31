fb30ca69430e701803461a6247814f1e6477e256
Simone <simonecaplin@gmail.com>
2018-05-31 14:27:00 +0100

    Add web helper for sign in and play

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,6 +23,7 @@ RSpec.configure do |config|
   require 'capybara'
   require 'capybara/rspec'
   require 'rspec'
+  require 'features/web_helpers'
 
   # tell Capybara about our app class
   Capybara.app = Battle
